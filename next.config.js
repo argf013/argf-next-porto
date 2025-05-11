24017b87226f2b6afd84ec8267bf9f8ad26ed7a1
argf013 <alghi344@gmail.com>
2025-05-11 19:36:32 +0700

    chore: add prettier config and next config files chore: update package.json to include prettier and next dependencies

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['005027.sgp1.vultrobjects.com'],
+  },
+
+  experimental: {
+    optimizePackageImports: ['react', 'react-dom'],
+
+    optimizeCss: true,
+  },
+
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === 'production'
+        ? {
+            exclude: ['error', 'warn'],
+          }
+        : false,
+  },
+  poweredByHeader: false,
+}
+
+module.exports = nextConfig
