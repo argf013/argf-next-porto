ccaf2f25341fddfa8308f6257348fb24ed961c7f
argf013 <alghi344@gmail.com>
2025-05-20 13:37:27 +0700

    init sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://argf.web.id/'
+
+  const routes = ['', '/blog', '/experience', '/projects', '/pixel-gallery']
+
+  const sitemapEntries = routes.map(route => {
+    return {
+      url: `${baseUrl}${route}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: route === '' ? 1 : 0.8,
+    }
+  })
+
+  return sitemapEntries
+}
