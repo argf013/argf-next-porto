3e0abe321fa3121a945fbf66a7c7b70042c824a3
argf013 <alghi344@gmail.com>
2025-07-14 04:28:49 +0700

    feat: add sec keys element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,18 @@ import TechStack from '@/components/TechStack'
 
 export default function Home() {
   return (
-    <div className='flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]'>
-      <section className='mx-auto flex flex-row items-center mt-10 p-[64px] md:px-[200px] gap-[40px] w-full md:w-[1280px] h-[392px]'>
+    <div className='flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)] relative'>
+      <div className='absolute top-0 right-0 lg:top-[-100px] lg:right-0 block'>
+        <Image
+          src='/seq-keys.webp'
+          alt='Floating keycaps animation'
+          width={600}
+          height={600}
+          unoptimized
+        />
+      </div>
+
+      <section className='mx-auto flex flex-row items-center mt-10 p-[64px] md:px-[200px] gap-[40px] w-full md:w-[1280px] h-[392px] relative z-10'>
         <div className='flex flex-col gap-[20px]'>
           <h1 className='text-4xl font-bold mt-25 md:mt-0'>Hi There!</h1>
           <div className='flex flex-col gap-2'>
@@ -25,7 +35,13 @@ export default function Home() {
             </a>
           </div>
         </div>
-        <div className='hidden md:block h-[250px] w-[250px] rounded-full overflow-hidden flex-shrink-0'>
+        <div
+          className='hidden md:block h-[250px] w-[250px] rounded-full overflow-hidden flex-shrink-0'
+          style={{
+            filter:
+              'drop-shadow(0 0 20px rgba(255, 255, 255, 0.3)) drop-shadow(0 0 40px rgba(255, 255, 255, 0.2))',
+          }}
+        >
           <Image
             className='object-cover w-full h-full'
             src={arfaProfile}
